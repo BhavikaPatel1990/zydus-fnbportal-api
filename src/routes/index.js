ca61343dd481ce6a2fa6cd4d6efadbdcce4f69aa
Bhavika Patel <bhavujaviya@gmail.com>
2026-05-13 14:52:54 +0530

    authe check

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import canteenRoutes from './canteen/index.js';
 import ipdRoutes from './ipd/index.js';
 import profileRoutes from './profile/profile.route.js';
+import { authorize } from '../middleware/authorize.js';
 
 const router = Router();
 
@@ -9,8 +10,17 @@ router.get('/', (req, res) => {
     res.send('FNB API running');
 });
 
+// Session verification route
+router.get('/auth/me', authorize, (req, res) => {
+    res.json({
+        success: true,
+        user: req.user
+    });
+});
+
 router.use('/profile', profileRoutes);
 router.use('/canteen', canteenRoutes);
 router.use('/ipd', ipdRoutes);
 
 export default router;
+
